8d9660afd25e1e506c407718ce5519ee5bb00970
Michael Virgo <mvirgs44@gmail.com>
2018-12-18 10:57:22 -0800

    fix: change to unsigned int to remove warning
    Added an unsigned int to a loop in CalculateRMSE to remove the warning occurring.

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,19 +14,19 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   VectorXd rmse(4);
   rmse << 0,0,0,0;
   
-  //accumulate squared residuals
-  for(int i=0; i < estimations.size(); ++i){
+  // accumulate squared residuals
+  for (unsigned int i=0; i < estimations.size(); ++i) {
     VectorXd residuals = estimations[i] - ground_truth[i];
     residuals = residuals.array() * residuals.array();
     rmse += residuals;
   }
   
-  //calculate the mean
+  // calculate the mean
   rmse = rmse / estimations.size();
   
-  //calculate the squared root
+  // calculate the squared root
   rmse = sqrt(rmse.array());
   
-  //return the result
+  // return the result
   return rmse;
 }
